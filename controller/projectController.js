96d7b4a0e7b2680e60e419d89222b24c9c87720e
Manjitha-MK <manjithataridu123@gmail.com>
2025-06-30 13:49:03 +0530

    created deleteProject function 😎👍

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,27 +1,44 @@
 import Project from "../models/projects.js";
 
-export function createProject(req, res){
+export function createProject(req, res) {
+  const newProductData = req.body;
 
-    const newProductData = req.body;
+  const project = new Project(newProductData);
 
-    const project = new Project(newProductData);
-
-    project.save().then(() => {
-        res.json({
-            message: "Project created",
-        });
-    }).catch((error) => {
-        res.status(403).json({
-            message: error,
-        });
+  project
+    .save()
+    .then(() => {
+      res.json({
+        message: "Project created",
+      });
+    })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
     });
-
 }
 
-export function getProject(req, res){
-    Project.find({}).then((projects) => {
-        res.json({
-            projects,
-        });
+export function getProject(req, res) {
+  Project.find({}).then((projects) => {
+    res.json({
+      projects,
+    });
+  });
+}
+
+export function deleteProject(req, res) {
+  const projectId = req.params.projectId;
+
+  Product.deleteOne({ productId: projectId })
+    .then(() => {
+      res.json({
+        message: "Project deleted",
+      });
+    })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
     });
 }
