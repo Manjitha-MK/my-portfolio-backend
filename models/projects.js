2121a71482ac6638c2dc4eed5013d618da48ec0d
Manjitha-MK <manjithataridu123@gmail.com>
2025-07-11 11:43:52 +0530

    update Mongoose schema to support technologies as array and validate project & GitHub URLs

--- models/projects.js
+++ models/projects.js
@@ -1,26 +1,54 @@
 import mongoose from "mongoose";
 
 const projectSchema = mongoose.Schema({
-    projectId : {
-        type : String,
-        required : true,
-        unique : true
+  projectId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  projectName: {
+    type: String,
+    required: true,
+  },
+  Images: [
+    {
+      type: String,
     },
-    projectName : {
-        type : String,
-        required : true
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  technologies: {
+    type: [String],
+    required: true,
+  },
+  projectUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/.+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL`,
     },
-    Images : [
-        {
-            type : String
-        }
-    ],
-    description : {
-        type : String,
-        required : true
-    }
+  },
+  githubUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/(www\.)?github\.com\/.+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid GitHub URL`,
+    },
+  },
 });
 
-const Project = mongoose.model("projects",projectSchema)
+const Project = mongoose.model("projects", projectSchema);
 
-export default Project
+export default Project;
