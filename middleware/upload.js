234125c95fd9160aba2e98563f571598984fe61d
Manjitha-MK <manjithataridu123@gmail.com>
2025-07-18 13:10:20 +0530

    install multer and create middleware folder(upload.js) and create upload folder

---
+++ middleware/upload.js
@@ -0,0 +1,28 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (ext === ".jpg" || ext === ".jpeg" || ext === ".png" || ext === ".webp") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
+
+export default upload;
