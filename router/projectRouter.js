a0e1755acaaef5ace39f67a2ddb6b03bbe951e03
Manjitha-MK <manjithataridu123@gmail.com>
2025-07-21 13:44:00 +0530

    Add image upload support using Multer for project creation and update

--- router/projectRouter.js
+++ router/projectRouter.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { createProject, deleteProject, getProject, getProjectById, updateProject } from "../controller/projectController.js";;
-
+import {
+  createProject,
+  deleteProject,
+  getProject,
+  getProjectById,
+  updateProject,
+} from "../controller/projectController.js";
+import upload from "../middleware/upload.js";
 const projectRouter = express.Router();
 
-projectRouter.post("/", createProject);
+// projectRouter.post("/", createProject);
 projectRouter.get("/", getProject);
 projectRouter.get("/:projectId", getProjectById);
-projectRouter.put("/:projectId",updateProject);
-projectRouter.delete("/:projectId",deleteProject);
+projectRouter.put(
+  "/:projectId",
+  upload.fields([{ name: "newImages", maxCount: 5 }]),
+  updateProject
+);
+projectRouter.delete("/:projectId", deleteProject);
+projectRouter.post("/", upload.array("images", 5), createProject);
 
 export default projectRouter;
